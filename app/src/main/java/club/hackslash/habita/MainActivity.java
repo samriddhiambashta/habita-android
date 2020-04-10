ecf633d12ea0edcb4027263f9aaf8a39440fed91
samriddhiambashta <ambashtasamriddhi@gmail.com>
2020-04-10 16:41:24 +0530

    Charging status notifier added

--- app/src/main/java/club/hackslash/habita/MainActivity.java
+++ app/src/main/java/club/hackslash/habita/MainActivity.java
@@ -2,13 +2,45 @@ package club.hackslash.habita;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private MyBattery myBattery;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myBattery= new MyBattery();
     }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        IntentFilter intentFilter= new IntentFilter(Intent.ACTION_POWER_DISCONNECTED);
+        intentFilter.addAction(Intent.ACTION_POWER_CONNECTED);
+        intentFilter.addAction(Intent.ACTION_BATTERY_LOW);
+
+
+
+
+
+        registerReceiver(myBattery,intentFilter);
+        //to register broadcast receiver
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        unregisterReceiver(myBattery);
+
+    }
+
+
+
+
+
 }
